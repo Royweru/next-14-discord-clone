0d2f6be0ef4c80ae24e45a997a84cc1a500b8158
Royweru <royezi17@gmail.com>
2023-11-15 07:20:03 +0300

    new file:   .eslintrc.json 	new file:   .gitignore 	new file:   app/(auth)/layout.tsx 	new file:   app/(main)/page.tsx 	new file:   app/favicon.ico 	new file:   app/globals.css 	new file:   app/layout.tsx 	new file:   components.json 	new file:   components/ui/button.tsx 	new file:   lib/utils.ts     new file:   next.config.js 	new file:   package-lock.json 	new file:   package.json 	new file:   postcss.config.js 	new file:   public/next.svg 	new file:   public/vercel.svg 	new file:   tailwind.config.js 	new file:   tailwind.config.ts 	new file:   tsconfig.json

---
+++ app/(main)/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className=' text-xl font-bold text-blue-50'>
+          This is the dicord clone
+        </div>
+    </main>
+  )
+}
