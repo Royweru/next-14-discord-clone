cbbb1e05649293ffaa71e80e4c8630c87829755b
Royweru <royezi17@gmail.com>
2023-12-16 12:07:08 +0300

    modified:   app/api/members/[memberId]/route.ts 	modified:   components/modals/members-modal.tsx

--- app/api/members/[memberId]/route.ts
+++ app/api/members/[memberId]/route.ts
@@ -1,6 +1,59 @@
 import { currentProfile } from "@/lib/current-profile";
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prismadb"
+
+
+export async function DELETE(
+    req:Request,
+    {params}:{params: {memberId:string}}
+) {
+    try {
+        const profile = await currentProfile()
+        const {searchParams} = new URL(req.url)
+
+        const serverId = searchParams.get("serverId")
+        if(!profile){
+            return new NextResponse("unauthorized", {status:401})
+        }
+        if(!serverId){
+            return new NextResponse("Server ID is missing", {status:400})
+        }
+
+        if(!params.memberId){
+            return new NextResponse("Member ID is missing",{status:400})
+        }
+        const server = await prisma.server.update({
+            where:{
+                id:serverId,
+                profileId:profile.id
+            },
+            data:{
+                members:{
+                    deleteMany:{
+                        id:params.memberId,
+                        profileId:{
+                            not:profile.id
+                        }
+                    }
+                }
+            },
+            include:{
+                members:{
+                    include:{
+                        profile:true
+                    },
+                    orderBy:{
+                        role:"asc"
+                    }
+                },
+            },
+        })
+        return NextResponse.json(server)
+    } catch (error) {
+        console.log("[MEMBER_ID_DELETE",error)
+        return new NextResponse("internal error", {status:500})
+    }
+}
 export async function PATCH(
     req:Request,
     {params}:{params:{memberId:string}}
