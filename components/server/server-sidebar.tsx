8fe98531eb1e33573c09fb61af629442844e1fe6
Royweru <royezi17@gmail.com>
2024-01-13 12:32:34 +0300

    new file:   components/server/server-channel.tsx 	renamed:    components/server-search.tsx -> components/server/server-search.tsx 	new file:   components/server/server-section.tsx 	modified:   components/server/server-sidebar.tsx

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -5,9 +5,12 @@ import React from 'react'
 import prisma from "@/lib/prismadb"
 import { ChannelType, MemberRole } from '@prisma/client'
 import { ScrollArea } from '../ui/scroll-area'
+import { Separator } from '../ui/separator'
 
 import ServerHeader from './server-header'
-import { ServerSearch } from '../server-search'
+import { ServerSection } from './server-section'
+import { ServerSearch } from './server-search'
+import { ServerChannel } from './server-channel'
 import { Hash, Mic, ShieldAlert, ShieldCheck, Video } from 'lucide-react'
 interface ServerSideBarProps{
     serverId:string
@@ -73,6 +76,7 @@ const ServerSideBar:React.FC<ServerSideBarProps> = async({
     <div className=' flex flex-col h-full text-primary dark:bg-[#2B2D31] bg-[#F2F3F5]'>
       <ServerHeader server={server} role={role} />
       <ScrollArea className=' flex-1 px-3'>
+        <div className=' mt-2'>
            <ServerSearch
             data={[
                 {
@@ -121,6 +125,26 @@ const ServerSideBar:React.FC<ServerSideBarProps> = async({
                 }
             ]}
            />
+           </div>
+           <Separator className=' bg-zinc-200 dark:bg-zinc-700 rounded-md my-2'/>
+          {!!textChannels?.length&&(
+            <div className=' mb-2'>
+                <ServerSection
+                 sectionType='channels'
+                 channelType={ChannelType.TEXT}
+                 role={role}
+                 label='Text Channels'
+                />
+                {textChannels.map((channel)=>(
+                    <ServerChannel
+                     key={channel.id}
+                     role={role}
+                     channel={channel}
+                     server={server}
+                    />
+                ))}
+            </div>
+          )}
       </ScrollArea>
     </div>
   )
